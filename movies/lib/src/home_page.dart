235295dffe199988e89c9b9bfbb8ba0219e2ea76
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2019-11-21 11:38:36 -0500

    First commit

---
+++ movies/lib/src/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Peliculas en cine'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemHeight: 200.0,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: SwiperPagination(),
+        control: SwiperControl(),
+      ),
+    );
+  }
+}
