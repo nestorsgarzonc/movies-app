8b72041cb49d11785986be72e23e577ee8e5f9fb
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2019-11-28 22:40:43 -0500

    search delegate

---
+++ movies/lib/src/search/search_delegate.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    //Acciones de nuestro AppBar
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    //Icono a la izquierda
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    //Resultados
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //Sugerencias busqueda
+    return null;
+  }
+(){
+
+}
